33c567570bef05049ba7f89c6abbc105e78ad5c3
nana-4 <hnmaigo@gmail.com>
2016-11-23 00:00:21 +0900

    Add install message a bit more

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo installing...
-./scripts/install-theme.sh
-./scripts/install-theme-compact.sh
-#./scripts/install-theme-dark.sh
-#./scripts/install-theme-dark-compact.sh
-./scripts/install-theme-light.sh
-./scripts/install-theme-light-compact.sh
-echo done.
+echo
+echo Installing Flat-Plat && ./scripts/install-theme.sh
+echo Installing Flat-Plat-compact && ./scripts/install-theme-compact.sh
+# echo Installing Flat-Plat-dark && ./scripts/install-theme-dark.sh
+# echo Installing Flat-Plat-dark-compact && ./scripts/install-theme-dark-compact.sh
+echo Installing Flat-Plat-light && ./scripts/install-theme-light.sh
+echo Installing Flat-Plat-light-compact && ./scripts/install-theme-light-compact.sh
+echo
+echo Done.
