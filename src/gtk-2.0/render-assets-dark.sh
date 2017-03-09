99412caea8c9861ada823689f1d9eaf55e162427
nana-4 <hnmaigo@gmail.com>
2017-03-10 01:57:44 +0900

    GTK2: Add support for dark theme

--- src/gtk-2.0/render-assets-dark.sh
+++ src/gtk-2.0/render-assets-dark.sh
@@ -3,8 +3,8 @@
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
+SRC_FILE="assets-dark.svg"
+ASSETS_DIR="assets-dark"
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
