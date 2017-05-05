50ecef89a51f3137736621caf96beec08f6f4a2f
nana-4 <hnmaigo@gmail.com>
2017-05-05 20:24:41 +0900

    parse-sass.sh: Pick up the upstream code partially
    from https://git.gnome.org/browse/gtk+/commit/?id=96fc66d32761e8224224d172cdcefc3fb7efab15

--- parse-sass.sh
+++ parse-sass.sh
@@ -1,16 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ ! "$(which sassc 2> /dev/null)" ]; then
+  echo sassc needs to be installed to generate the css.
+  exit 1
+fi
+
+SASSC_OPT="-M -t expanded"
+
+echo Generating the css...
 
 for color in '' '-dark' '-light' ; do
-  sassc -t expanded src/gtk-3.0/3.18/gtk${color}.{scss,css}
+  sassc $SASSC_OPT src/gtk-3.0/3.18/gtk${color}.{scss,css}
 
   for size in '' '-compact' ; do
     for version in '3.20' '3.22' ; do
-      sassc -t expanded src/gtk-3.0/${version}/gtk${color}${size}.{scss,css}
+      sassc $SASSC_OPT src/gtk-3.0/${version}/gtk${color}${size}.{scss,css}
     done
 
     # This gnome-shell theme can skip versions '3.20' & '2.22'
     for version in '3.18' '3.24' ; do
-      sassc -t expanded src/gnome-shell/${version}/gnome-shell${color}${size}.{scss,css}
+      sassc $SASSC_OPT src/gnome-shell/${version}/gnome-shell${color}${size}.{scss,css}
     done
   done
 done
