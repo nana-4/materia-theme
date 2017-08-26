bc877d4c762414c9e4b822f08111211413896441
actionless <actionless.loveless@gmail.com>
2017-08-26 18:36:56 +0200

    feat(gtk2, gtk3: render-assets): use `parallel` if installed

---
+++ src/gtk-2.0/render-asset.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+INKSCAPE="/usr/bin/inkscape"
+OPTIPNG="/usr/bin/optipng"
+
+SRC_FILE="assets.svg"
+ASSETS_DIR="assets"
+
+i=${1}
+
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-background-opacity=0 \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+fi
+
+exit 0
--- src/gtk-3.0/gtk-common/render-assets.sh
+++ src/gtk-3.0/gtk-common/render-assets.sh
@@ -1,34 +1,16 @@
 #! /bin/bash
+set -ueo pipefail
 
-INKSCAPE="/usr/bin/inkscape"
-OPTIPNG="/usr/bin/optipng"
-
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
 INDEX="assets.txt"
 
-for i in `cat $INDEX`
-do 
-if [ -f $ASSETS_DIR/$i.png ]; then
-    echo $ASSETS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
-    $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null #\
-    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-fi
-if [ -f $ASSETS_DIR/$i@2.png ]; then
-    echo $ASSETS_DIR/$i@2.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i@2.png
-    $INKSCAPE --export-id=$i \
-              --export-dpi=192 \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null #\
-    # && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
-fi
-done
+_parallel() {
+	which parallel && parallel $@ || (
+		while read i;
+		do
+			$1 $i
+		done
+	)
+}
+
+cat $INDEX | _parallel ./render-asset.sh
 exit 0
