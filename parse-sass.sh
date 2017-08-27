e29430645a962255c38286be8e5b68244d3ceede
Yauhen Kirylau <actionless.loveless@gmail.com>
2017-08-27 07:07:11 +0200

    feat(parse-sass.sh): allow to configure which theme variants to compile via env vars (#135)

--- parse-sass.sh
+++ parse-sass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ueo pipefail
 
 if [ ! "$(which sassc 2> /dev/null)" ]; then
   echo sassc needs to be installed to generate the css.
@@ -7,12 +8,28 @@ fi
 
 SASSC_OPT="-M -t expanded"
 
-echo Generating the css...
+_COLOR_VARIANTS=(
+	''
+	'-dark'
+	'-light'
+)
+if [ ! -z "${COLOR_VARIANTS:-}" ] ; then
+	IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
+fi
+_SIZE_VARIANTS=(
+	''
+	'-compact'
+)
+if [ ! -z "${SIZE_VARIANTS:-}" ] ; then
+	IFS=', ' read -r -a _SIZE_VARIANTS <<< "${SIZE_VARIANTS:-}"
+fi
 
-for color in '' '-dark' '-light' ; do
+echo "== Generating the CSS..."
+
+for color in "${_COLOR_VARIANTS[@]}" ; do
   sassc $SASSC_OPT src/gtk-3.0/3.18/gtk${color}.{scss,css}
 
-  for size in '' '-compact' ; do
+  for size in "${_SIZE_VARIANTS[@]}" ; do
     for version in '3.20' '3.22' ; do
       sassc $SASSC_OPT src/gtk-3.0/${version}/gtk${color}${size}.{scss,css}
     done
