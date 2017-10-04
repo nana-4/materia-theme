aca44ae8c47512efb1ed58d0e226baa545ba29aa
nana-4 <hnmaigo@gmail.com>
2017-10-04 21:53:51 +0900

    parse-sass.sh: Code cosmetics

--- parse-sass.sh
+++ parse-sass.sh
@@ -6,36 +6,37 @@ if [ ! "$(which sassc 2> /dev/null)" ]; then
   exit 1
 fi
 
-SASSC_OPT="-M -t expanded"
-
 _COLOR_VARIANTS=(
-	''
-	'-dark'
-	'-light'
+  ''
+  '-dark'
+  '-light'
 )
-if [ ! -z "${COLOR_VARIANTS:-}" ] ; then
-	IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
+if [ ! -z "${COLOR_VARIANTS:-}" ]; then
+  IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
 fi
+
 _SIZE_VARIANTS=(
-	''
-	'-compact'
+  ''
+  '-compact'
 )
-if [ ! -z "${SIZE_VARIANTS:-}" ] ; then
-	IFS=', ' read -r -a _SIZE_VARIANTS <<< "${SIZE_VARIANTS:-}"
+if [ ! -z "${SIZE_VARIANTS:-}" ]; then
+  IFS=', ' read -r -a _SIZE_VARIANTS <<< "${SIZE_VARIANTS:-}"
 fi
+
+SASSC_OPT="-M -t expanded"
 
 echo "== Generating the CSS..."
 
-for color in "${_COLOR_VARIANTS[@]}" ; do
+for color in "${_COLOR_VARIANTS[@]}"; do
   sassc $SASSC_OPT src/gtk-3.0/3.18/gtk${color}.{scss,css}
 
-  for size in "${_SIZE_VARIANTS[@]}" ; do
-    for version in '3.20' '3.22' ; do
+  for size in "${_SIZE_VARIANTS[@]}"; do
+    for version in '3.20' '3.22'; do
       sassc $SASSC_OPT src/gtk-3.0/${version}/gtk${color}${size}.{scss,css}
     done
 
     # This gnome-shell theme can skip versions '3.20' & '2.22'
-    for version in '3.18' '3.24' '3.26' ; do
+    for version in '3.18' '3.24' '3.26'; do
       sassc $SASSC_OPT src/gnome-shell/${version}/gnome-shell${color}${size}.{scss,css}
     done
   done
