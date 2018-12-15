7f252e74d43fe6bfd5673f7c308d41be63c14272
actionless <actionless.loveless@gmail.com>
2018-12-15 19:22:16 +0100

    feat(render_assets): optional support for resvg

--- src/chrome/render-asset.sh
+++ src/chrome/render-asset.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -ueo pipefail
 
-INKSCAPE="$(which inkscape)"
-OPTIPNG="$(which optipng)"
+RENDER_SVG="$(command -v rendersvg)"
+INKSCAPE="$(command -v inkscape)"
+OPTIPNG="$(command -v optipng)"
 
 i="$1"
+GTK2_HIDPI="$(echo "${GTK2_HIDPI-False}" | tr '[:upper:]' '[:lower:]')"
+if [[ "${GTK2_HIDPI}" == "true" ]] ; then
+  DPI=192
+else
+  DPI=96
+fi
 
 echo "Rendering '$i.png'"
-"$INKSCAPE" --export-png="$i.png" "$i.svg" >/dev/null \
-&& "$OPTIPNG" -o7 --quiet "$i.png"
+if [[ -n "${RENDER_SVG}" ]] ; then
+  "$RENDER_SVG" --dpi ${DPI} "$i.svg" "$i.png"
+else
+  "$INKSCAPE" --export-dpi=${DPI} --export-png="$i.png" "$i.svg" >/dev/null
+fi
+
+"$OPTIPNG" -o7 --quiet "$i.png"
--- src/gtk-2.0/render-asset.sh
+++ src/gtk-2.0/render-asset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -ueo pipefail
 
-INKSCAPE="$(which inkscape)"
-OPTIPNG="$(which optipng)"
+RENDER_SVG="$(command -v rendersvg)"
+INKSCAPE="$(command -v inkscape)"
+OPTIPNG="$(command -v optipng)"
 
 if [[ "$1" == "dark" ]]; then
   SRC_FILE="assets-dark.svg"
@@ -16,14 +17,21 @@ i="$2"
 
 GTK2_HIDPI="$(echo "${GTK2_HIDPI-False}" | tr '[:upper:]' '[:lower:]')"
 if [[ "${GTK2_HIDPI}" == "true" ]] ; then
-  EXTRA_OPTIONS=("--export-dpi=192")
+  DPI=192
 else
-  EXTRA_OPTIONS=()
+  DPI=96
 fi
 
 echo "Rendering '$ASSETS_DIR/$i.png'"
-"$INKSCAPE" --export-id="$i" \
-            --export-id-only \
-            ${EXTRA_OPTIONS+"${EXTRA_OPTIONS[@]}"} \
-            --export-png="$ASSETS_DIR/$i.png" "$SRC_FILE" >/dev/null \
-&& "$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i.png"
+if [[ -n "${RENDER_SVG}" ]] ; then
+  "$RENDER_SVG" --export-id "$i" \
+        --dpi ${DPI} \
+         "$SRC_FILE" "$ASSETS_DIR/$i.png"
+else
+  "$INKSCAPE" --export-id="$i" \
+        --export-id-only \
+        --export-dpi=${DPI} \
+        --export-png="$ASSETS_DIR/$i.png" "$SRC_FILE" >/dev/null
+fi
+
+"$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i.png"
